9cdb023d8004303a11527550eda726e801ed42bb
muneeebahmed15 <muneebahme999@gmail.com>
2024-04-30 23:39:20 +0500

    x

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react'
 import Layout from './Components/Dashboard/Layout'
 import ColorChanger from './Components/Hooks/ColorChanger'
 import LeftSiderBar from './Components/Dashboard/LeftSiderBar'
+import Login from './login'
 
 const App = () => {
   return (
     <>
-      <Layout/>
-      <ColorChanger/>
+      {/* <Layout/>
+    
+      <ColorChanger/> */}
+      <Login/>
     </>
   )
 }
